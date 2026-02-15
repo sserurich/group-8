c2f062b6e4062f7f6dfebeb7143020fa7d741bc3
Connor-Palmira <palmic1@unlv.nevada.edu>
2026-02-15 10:18:44 -0800

    Add test Return 404 for non-existent counter

--- tdd_lab/tests/test_counter.py
+++ tdd_lab/tests/test_counter.py
@@ -10,3 +10,27 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+import pytest
+from src import app
+from src import status
+
+@pytest.fixture()
+def client():
+    """Fixture for Flask test client"""
+    return app.test_client()
+
+@pytest.mark.usefixtures("client")
+class TestCounterEndpoints:
+    """Test cases for Counter API"""
+
+    def test_create_counter(self, client):
+        """It should create a counter"""
+        result = client.post('/counters/foo')
+        assert result.status_code == status.HTTP_201_CREATED
+
+    def test_get_nonexistent_counter(self, client):
+        """It should return 404 if counter does not exist"""
+
+        result = client.get('/counters/ghost')
+
+        assert result.status_code == status.HTTP_404_NOT_FOUND
